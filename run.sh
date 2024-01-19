e5572ef2ed61886d064398175f2301ef911be98e
KiRzEa <107606777+KiRzEa@users.noreply.github.com>
2024-01-19 22:39:05 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 # Install dependencies
 pip install -r requirements.txt --quiet
 # Unzip example videos
-unzip video.zip
+mkdir -p video
+unzip -n video.zip -d video
 # Run and build the interface for inference
 python app.py
