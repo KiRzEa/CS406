9282cbd8bded6e248dadc0d8a49068df7ab09b45
KiRzEa <107606777+KiRzEa@users.noreply.github.com>
2024-01-19 22:41:40 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Install dependencies
 pip install -r requirements.txt --quiet
+# Unzip weight file
+unzip -n weights/best.zip -d weights
 # Unzip example videos
 mkdir -p video
 unzip -n video.zip -d video
